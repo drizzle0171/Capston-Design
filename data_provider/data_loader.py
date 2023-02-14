a23dc1b313910f618d8904cd6fbd7f4a7fc34cc7
drizzle0171 <yongsandew@gmail.com>
2023-02-14 22:10:30 +0900

    modify ETT

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -10,6 +10,90 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
+class Dataset_Mirae_H(Dataset):
+    def __init__(self, root_path, flag='train', size=None,
+                 features='S', data_path='ETTh1.csv',
+                 target='OT', scale=True, timeenc=0, freq='h', rep=None):
+        # size [seq_len, label_len, pred_len]
+        # info
+        if size == None:
+            self.seq_len = 24 * 4 * 4
+            self.label_len = 24 * 4
+            self.pred_len = 24 * 4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+        # init
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
+
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.timeenc = timeenc
+        self.freq = freq
+
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        x = np.load('/nas/datahub/mirae/Data/x_H_total.npy')
+        y = np.load('/nas/datahub/mirae/Data/y_H_total.npy')
+        time = np.load('/nas/datahub/mirae/Data/time_H_index.npy')
+
+        '''
+        df_raw.columns: ['date', ...(other features), target feature]
+        '''
+        num_train = int(len(x) * 0.7)
+        num_val = int(len(x) * 0.85)
+        border1s = [0, num_train, num_val]
+        border2s = [num_train, num_val, len(x)]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        # if self.features == 'M' or self.features == 'MS':
+        #     cols_data = df_raw.columns[1:]
+        #     df_data = df_raw[cols_data]
+        # elif self.features == 'S':
+        #     df_data = df_raw[[self.target]]
+
+        train_data = x[border1s[0]:border2s[0]]
+        max_x = np.max(train_data)
+        min_x = np.min(train_data)
+        x = (x - min_x) / (max_x - min_x)
+        y = (y - min_x) / (max_x - min_x)
+        
+        df_stamp = time[border1:border2]
+        month = np.array(list(map(lambda x: pd.to_datetime(x).month, df_stamp))).reshape(df_stamp.shape[0],df_stamp.shape[1],1)
+        day = np.array(list(map(lambda x: pd.to_datetime(x).day, df_stamp))).reshape(df_stamp.shape[0],df_stamp.shape[1],1)
+        weekday = np.array(list(map(lambda x: pd.to_datetime(x).weekday, df_stamp))).reshape(df_stamp.shape[0],df_stamp.shape[1],1)
+        hour = np.array(list(map(lambda x: pd.to_datetime(x).hour, df_stamp))).reshape(df_stamp.shape[0],df_stamp.shape[1],1)
+        data_stamp = np.concatenate((month,day,weekday,hour),axis=-1)
+
+        self.data_x = x[border1:border2].reshape(-1, x.shape[1], 1)
+        self.data_y = y[border1:border2].reshape(-1, y.shape[1], 1)
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        s_end = self.seq_len
+        # r_begin = s_end - self.label_len
+        r_end = self.seq_len + self.pred_len
+
+        seq_x = self.data_x[index]
+        seq_y = self.data_y[index]
+        seq_x_mark = self.data_stamp[index][:s_end]
+        seq_y_mark = self.data_stamp[index][s_end:r_end]
+        
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x)
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
 
 class Dataset_ETT_hour(Dataset):
     def __init__(self, root_path, flag='train', size=None,
@@ -33,7 +117,7 @@ class Dataset_ETT_hour(Dataset):
         self.features = features
         self.target = target
         self.scale = scale
-        self.timeenc = timeenc
+        self.timeenc = 0
         self.freq = freq
 
         self.root_path = root_path
@@ -62,15 +146,17 @@ class Dataset_ETT_hour(Dataset):
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
-
+        
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        
         if self.timeenc == 0:
             df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
             df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
             df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
             df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
             data_stamp = df_stamp.drop(['date'], 1).values
+            
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
@@ -78,7 +164,6 @@ class Dataset_ETT_hour(Dataset):
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
-
     def __getitem__(self, index):
         s_begin = index
         s_end = s_begin + self.seq_len
